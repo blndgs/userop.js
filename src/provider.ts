1dcfe939dac7fd417c551370b9ce5d36389dd1bd
singhhp1069 <singhhp1069@gmail.com>
2024-11-14 14:42:09 +0100

    fix: userop custom method

--- src/provider.ts
+++ src/provider.ts
@@ -45,44 +45,50 @@ export class BundlerJsonRpcProvider extends ethers.providers.JsonRpcProvider {
     return this;
   }
 
- /**
-   * Internal method to make a JSON-RPC request with a custom User-Agent header.
-   * Overrides the default fetchJson behavior in JsonRpcProvider to add specific HTTP headers.
+  /**
+   * Internal method to send JSON-RPC requests with custom headers.
    * 
-   * This method is intended for internal use within JsonRpcProvider and should not be called directly.
-   * 
-   * @param {string} connection - The URL of the RPC connection.
-   * @param {string} json - The JSON-RPC payload to be sent.
-   * @returns {Promise<any>} A promise that resolves to the response data from the RPC request.
-   * @throws Will throw an error if the RPC request fails.
+   * @param {string} method - The JSON-RPC method to be called.
+   * @param {any[]} params - The parameters for the JSON-RPC method.
+   * @returns {Promise<any>} A promise that resolves to the result of the RPC call.
+   * @throws {Error} When the RPC request fails.
    * @private
    */
-  async fetchJson(connection: string, json: string): Promise<any> {
-    const customAxios = axios.create({
-      headers: { "User-Agent": this.userAgent, "Content-Type": "application/json" },
-    });
+  async _send(method: string, params: any[]): Promise<any> {
+    const request = {
+      method: "POST",
+      url: this.connection.url,
+      headers: {
+        "Content-Type": "application/json",
+        "User-Agent": this.userAgent
+      },
+      data: JSON.stringify({
+        method,
+        params,
+        id: (new Date()).getTime(),
+        jsonrpc: "2.0"
+      })
+    };
 
     try {
-      const response = await customAxios.post(connection, json);
-      return response.data;
+      const response = await axios(request);
+      return response.data.result;
     } catch (error) {
       throw new Error(`RPC request failed: ${error}`);
     }
   }
 
   /**
-   * Sends a JSON-RPC request. If the method is a bundler-specific method, it uses the bundler RPC.
-   * Otherwise, it falls back to the parent provider's send method.
+   * Sends a JSON-RPC request, routing bundler-specific methods to the bundler RPC if configured.
    * 
    * @param {string} method - The JSON-RPC method to be called.
    * @param {any[]} params - The parameters for the JSON-RPC method.
    * @returns {Promise<any>} A promise that resolves to the result of the RPC call.
    */
-  send(method: string, params: any[]): Promise<any> {
+  async send(method: string, params: any[]): Promise<any> {
     if (this.bundlerRpc && this.bundlerMethods.has(method)) {
       return this.bundlerRpc.send(method, params);
     }
-
-    return super.send(method, params);
+    return this._send(method, params);
   }
 }
